c944e98882f55879b4a8331b53cf364ce75612ff
21A91A05E3 <97424858+21A91A05E3@users.noreply.github.com>
2023-02-01 11:47:35 +0530

    import java.util.*; class Multiplication {     public static void main(String args[])     {     int a,b;     Scanner sc=new Scanner(System.in);     a=sc.nextInt();     b=sc.nextInt();     float r=(float)(a+b)/2;     System.out.format("%.4f",r);     } }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Multiplication
+{
+    public static void main(String args[])
+    {
+    int a,b;
+    Scanner sc=new Scanner(System.in);
+    a=sc.nextInt();
+    b=sc.nextInt();
+    float r=(float)(a+b)/2;
+    System.out.format("%.4f",r);
+    }
+}
