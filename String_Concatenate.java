623adba654cad86d74b7f9f4089125330a654714
Harika Kannuri <97424858+21A91A05E3@users.noreply.github.com>
2023-06-24 09:57:26 +0530

    import java.util.*; public class concat {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String s1=sc.nextLine();         String s2=sc.nextLine();         s1=s1.concat(s2);         char arr[]=s1.toCharArray();         char temp;         for(int i=0;i<arr.length;i++)         {             for(int j=i+1;j<arr.length;j++)             {                 if(arr[i]>arr[j])                 {                     temp=arr[i];                     arr[i]=arr[j];                     arr[j]=temp;                 }             }         }         System.out.println(arr);
        }
    }

---
+++ String_Concatenate.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class concat
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s1=sc.nextLine();
+        String s2=sc.nextLine();
+        s1=s1.concat(s2);
+        char arr[]=s1.toCharArray();
+        char temp;
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=i+1;j<arr.length;j++)
+            {
+                if(arr[i]>arr[j])
+                {
+                    temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+        System.out.println(arr);
+        
+    }
+}
