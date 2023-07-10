a95e8bcf68f39613579aaa899c67ebb01461c098
Harika Kannuri <97424858+21A91A05E3@users.noreply.github.com>
2023-07-10 21:52:13 +0530

    import java.util.*; public class Conversion {     public static void main(String args[])     {         float c;         Scanner sc=new Scanner(System.in);         c=sc.nextFloat();         double f=(float)(32+(c*(1.8)));         System.out.format("%.2f",f);     } }

---
+++ Celsius_To_Fahrenheit.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Conversion
+{
+    public static void main(String args[])
+    {
+        float c;
+        Scanner sc=new Scanner(System.in);
+        c=sc.nextFloat();
+        double f=(float)(32+(c*(1.8)));
+        System.out.format("%.2f",f);
+    }
+}
