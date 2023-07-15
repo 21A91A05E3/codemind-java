449cad6e5fa4f55b4797c2e11524f2313894140a
Harika Kannuri <97424858+21A91A05E3@users.noreply.github.com>
2023-07-15 20:07:06 +0530

    import java.util.*; public class oddeven {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int a[]=new int[n];         int s=0;         for(int i=0;i<n;i++)         {             a[i]=sc.nextInt();             s+=a[i];         }         int avg=s/n;         int c=0;         for(int i=0;i<n;i++)         {             if(a[i]>=avg)c+=1;         }         System.out.println(c);
        }
    }

---
+++ Elements_greater_than_average.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class oddeven
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        int s=0;
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+            s+=a[i];
+        }
+        int avg=s/n;
+        int c=0;
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]>=avg)c+=1;
+        }
+        System.out.println(c);
+        
+    }
+}
