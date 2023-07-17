52c20e7a3c1626b57d1a75b2101909da74ebf297
Harika Kannuri <97424858+21A91A05E3@users.noreply.github.com>
2023-07-17 21:58:52 +0530

    import java.util.*; public class sumk {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int a[]=new int[n];         for(int i=0;i<n;i++)a[i]=sc.nextInt();         int k=sc.nextInt();         int s=0;         for(int i=0;i<k;i++)s+=a[i];         System.out.println(s);     } }

---
+++ sum_of_array__elements_upto__Kth__number_in_an_array.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class sumk
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)a[i]=sc.nextInt();
+        int k=sc.nextInt();
+        int s=0;
+        for(int i=0;i<k;i++)s+=a[i];
+        System.out.println(s);
+    }
+}
